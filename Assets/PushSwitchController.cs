4a7a20e2106d9f1464f68d6b5fe6fc7a9ec981b2
sasinn98 <sasinn98@gmail.com>
2016-04-11 16:45:26 +0700

    physics 555

---
+++ Assets/PushSwitchController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushSwitchController : MonoBehaviour {
+
+	public bool status;
+	public GameObject button;
+	bool isReady = true;
+
+	void OnCollisionEnter(Collision collision){
+		if (isReady) {
+			if (collision.gameObject == button) {
+				status = !status;
+				StartCoroutine (wait());
+			}
+		}
+	}
+
+	IEnumerator wait() {
+		isReady = false;
+		yield return new WaitForSeconds(5);
+		isReady = true;
+	}
+}
