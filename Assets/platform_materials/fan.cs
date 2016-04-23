ad91b0a9525a163357e4602559963f9346bfeb6a
Tino Thamjarat <lynx.tno@gmail.com>
2016-04-24 02:49:05 +0700

    Platform lvl 1 created

---
+++ Assets/platform_materials/fan.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fan : MonoBehaviour {
+	float rotationSpeed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (transform.position, Vector3.up, rotationSpeed);
+	}
+}
