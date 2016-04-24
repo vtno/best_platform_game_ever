d3a72373d6d5f028db0bc10d5bcf98d6fddfe13f
Tino Thamjarat <lynx.tno@gmail.com>
2016-04-24 16:17:42 +0700

    Revert "Platform lvl2"
    This reverts commit 3a7a578b0fd6d8c85134f70a95e00f709e6261ee.

--- Assets/Script/MovingWall.cs
+++ Assets/Script/MovingWall.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MovingWall : MonoBehaviour {
 
+	public GameObject mySwitch;
 	public bool isOpen;
 	public Transform openedPosition;
 	public Transform closedPosition;
@@ -15,6 +16,7 @@ public class MovingWall : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		isOpen = mySwitch.GetComponent<PushSwitchController> ().status;
 		if (isOpen) {
 			if (fraction < 1.0f) {
 				fraction += Time.deltaTime;
--- Assets/Script/PushSwitchController.cs
+++ Assets/Script/PushSwitchController.cs
@@ -5,16 +5,15 @@ public class PushSwitchController : MonoBehaviour {
 
 	public bool status;
 	public GameObject button;
-	protected bool isReady = true;
+	bool isReady = true;
 
 	void Start(){
 		UpdateColor ();
 	}
 
-	protected virtual void OnCollisionEnter(Collision collision){
+	void OnCollisionEnter(Collision collision){
 		if (isReady) {
 			if (collision.gameObject == button) {
-				Debug.Log("KUY");
 				status = !status;
 				UpdateColor ();
 				StartCoroutine (wait());
@@ -22,13 +21,13 @@ public class PushSwitchController : MonoBehaviour {
 		}
 	}
 
-	protected IEnumerator wait() {
+	IEnumerator wait() {
 		isReady = false;
 		yield return new WaitForSeconds(0.5f);
 		isReady = true;
 	}
 
-	protected void UpdateColor(){
+	void UpdateColor(){
 		if (status) {
 			this.GetComponent<Renderer> ().material.SetColor ("_Color", Color.green);
 			button.GetComponent<Renderer> ().material.SetColor ("_Color", Color.green);
