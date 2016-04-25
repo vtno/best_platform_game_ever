f66018195896b5ffa54ae94d4441fc4dff587193
Tino Thamjarat <lynx.tno@gmail.com>
2016-04-25 11:09:42 +0700

    Add pick up puzzle.

--- Assets/Script/openDoor.cs
+++ Assets/Script/openDoor.cs
@@ -4,19 +4,14 @@ using System.Collections;
 public class openDoor : PushSwitchController{
 	public GameObject wall1;
 	public GameObject wall2;
-	// Use this for initialization
-	void Start () {
-		
-	}	
 	
 	protected override void OnCollisionEnter(Collision collision){
 		if (isReady) {
-			if (collision.gameObject == button) {
-				wall1.GetComponent<MovingWall> ().isOpen = !wall1.GetComponent<MovingWall> ().isOpen;
-				Debug.Log (wall1.GetComponent<MovingWall> ().isOpen);
-				wall2.GetComponent<MovingWall> ().isOpen = !wall2.GetComponent<MovingWall> ().isOpen;
+			//if (collision.gameObject == button) {
+			wall1.GetComponent<MovingWall_noButton> ().isOpen = !wall1.GetComponent<MovingWall_noButton> ().isOpen;
+			wall2.GetComponent<MovingWall_noButton> ().isOpen = !wall2.GetComponent<MovingWall_noButton> ().isOpen;
 				StartCoroutine (wait());
-			}
+			//}
 		}
 	}
 		
