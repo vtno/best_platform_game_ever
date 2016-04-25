cdde4d06c19beb3d6d82e4d55c7630c34b7a5864
sasinn98 <sasinn98@gmail.com>
2016-04-25 14:29:06 +0700

    fucking done

--- Assets/Script/Finish.cs
+++ Assets/Script/Finish.cs
@@ -6,14 +6,16 @@ public class Finish : Pickup
 {
 
 	public int nextScene;
+	public bool finished = false;
 
 	public override void OnTriggerEnter (Collider collider)
 	{
 		if (collider.gameObject.tag.Equals ("Player") && collider.gameObject.GetComponent<PlayerKey> ().key) {
+			finished = true;
 			GetComponent<AudioSource> ().Play();
 			collider.gameObject.GetComponent<PlayerKey> ().key = false;
 			Debug.Log ("finished");
-			SceneManager.LoadScene (nextScene);
+			//SceneManager.LoadScene (nextScene);
 		}
 	}
 }
