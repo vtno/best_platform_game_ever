3149e6a405ed3e0edfb9fe5abb3b1b02135aba18
Tino Thamjarat <lynx.tno@gmail.com>
2016-05-09 04:12:17 +0700

    Create new puzzles

---
+++ Assets/Script/LevelTwo_Setting.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTwo_Setting : MonoBehaviour {
+
+	public GameObject item1;
+	public GameObject item2;
+	public GameObject item3;
+	public GameObject item4;
+	public GameObject checkpoint1;
+	public GameObject player;
+	public Transform startPosition;
+	public Transform checkpoint1Position;
+	public Material playerMaterial;
+
+	void Update() {
+		if (player.transform.position.y < -5f) {
+			Respawn ();
+		}
+	}
+
+	void Respawn() {
+		if (!checkpoint1.GetComponent<Checkpoint> ().isCurrent) {
+			player.GetComponent<Rigidbody> ().isKinematic = true;
+			player.transform.position = startPosition.position;
+			player.GetComponent<Rigidbody> ().isKinematic = false;
+		} else {
+			player.GetComponent<Rigidbody> ().isKinematic = true;
+			player.transform.position = checkpoint1Position.position;
+			player.GetComponent<Rigidbody> ().isKinematic = false;
+		}
+		player.GetComponent<Renderer> ().material.color = playerMaterial.color;
+		player.layer = 0;
+		player.GetComponent<PlayerKey> ().key = false;
+		player.GetComponent<AudioSource> ().Play ();
+		item1.SetActive (true);
+		item2.SetActive (true);
+		item3.SetActive (true);
+		item4.SetActive (true);
+	}
+
+}
---
+++ Assets/Script/wall_puzzle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class wall_puzzle : MonoBehaviour {
+	public GameObject player;
+	protected bool isReady = true;
+	protected bool isMoved = false;
+	public Transform openedPosition;
+	public Transform closedPosition;
+	private float fraction = 0f;
+	protected virtual void OnCollisionEnter(Collision collision){
+		if (isReady) {
+			Console.Write ("FUCK");
+			if (collision.gameObject == player) {
+				if (isMoved) {
+					if (fraction < 1.0f) {
+						fraction += Time.deltaTime;
+						isMoved = false;
+					}
+				} else {
+					if (fraction > 0.0f) {
+						fraction -= Time.deltaTime;
+						isMoved = true;
+					}
+				}
+			}
+		}
+		Mathf.Clamp (fraction, 0f, 1f);
+		transform.position = Vector3.Lerp (closedPosition.position, openedPosition.position, fraction);
+	}
+}
